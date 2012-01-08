4e4858f14da1a2c92c4fc7b262ae1aeb0a032151
Kurnia Muhamad <omenkzz@gmail.com>
2012-01-08 21:40:37 +0700

    pertamax

--- backend.js
+++ backend.js
@@ -1,2 +1,50 @@
-// wetonZZ -- backend.js
-log.info('Hello from backend bootstrap.');
+
+var jsdom = require('jsdom');
+
+var url = 'http://ki-demang.com';
+
+//jsdom.env(url,
+//	[
+//	'http://code.jquery.com/jquery-1.5.min.js'
+//	],
+//	function(errors, window) {
+//		window.$('.ptmain tr').each(function() {
+//			window.$(this).find("td").each(function() {
+//			    console.log(window.$(this).text());
+//			});
+//		});
+//	}
+//);
+
+jsdom.env(url,
+    [
+    'http://code.jquery.com/jquery-1.5.min.js'
+    ],
+    function(errors, window) {
+
+        var $ = window.jQuery;
+
+        // jQuery is now loaded on the jsdom window created from 'agent.body'
+        // console.log($('body').html());
+        //console.log($('body').html().find('tabel500').find("td").text());
+          console.log($('body').find('tabel500').html());
+
+    }
+    );
+
+
+//jsdom.env({
+//    html: 'http://news.ycombinator.com/',
+//    scripts: [
+//    'http://code.jquery.com/jquery-1.5.min.js'
+//    ],
+//    done: function(errors, window) {
+//        var $ = window.$;
+//        console.log('HN Links');
+//        $('td.title:not(:last) a').each(function() {
+//            console.log(' -', $(this).text());
+//        });
+//    }
+//});
+
+
