311bdae8c60fc7da165d9b59c462ad6b38255725
Kurnia Muhamad <omenkzz@gmail.com>
2012-01-09 19:05:51 +0700

    update backend n view

--- backend.js
+++ backend.js
@@ -1,27 +1,25 @@
-
 var http = require('http');
 var url = require('url');
 var QS = require('querystring');
 
 app.message(function(client, action, data) {
-    if (action === 'search') {
-      	
+        if (action ==='search'){
         var param = {
             'tg': data.tg,
             'bl': data.bl,
             'th': data.th
         };
         param = QS.stringify(param);
-		
+
+        //host : http://ki-demang.com/php_files/02%20kalender%20weton%20on%20line.php		
         var siteUrl = url.parse('http://ki-demang.com/php_files/kds10_02wetonproses.php');
-        console.log('url : ' + url);
+  
         var headers = {
             'Host': siteUrl.host,
             'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_1) AppleWebKit/535.2 (KHTML, like Gecko) Chrome/15.0.874.121 Safari/535.2',
             'Content-Type': 'application/x-www-form-urlencoded',
             'Content-Length': param.length
-        //'Cookie': 'key=value',
-        //....
+      
         };
 
         var site = http.createClient(siteUrl.port || 80, siteUrl.host);
@@ -34,7 +32,8 @@ app.message(function(client, action, data) {
             response.setEncoding('utf8');
             console.log('Status: ' + response.statusCode);
             response.on('data', function(data) {
-                console.log("Received Data: " + data);
+                console.log("Received Data (Data Masuk): " + data);
+                client.msg('search', data);
             });
         });
 
@@ -42,4 +41,24 @@ app.message(function(client, action, data) {
 });
 
 
+app.setResourceHandler(function(request, response) {
+
+    app.debug('Client requested resource-id=' + request.id);
+
+    function sendReply(response, error, imageType, data) {
+        if (error) {
+            app.warn('Failed to load image: ' + error);
+            response.failed();
+        } else {
+            app.debug('Loaded image.');
+            response.reply(imageType, data);
+        }
+    }
+
+    scaling.scale(request.id, request.display_width, request.display_height, 'image/jpeg',
+        function(err, data) {
+            sendReply(response, err, 'image/jpeg', data);
+        }
+        );
+});
 
--- controller/main.js
+++ controller/main.js
@@ -1,31 +1,55 @@
 var _ = require('common/util');
+var ImageView = require('ui').ImageView;
+var app = this;
 
 _.extend(exports, {
 	':load': function() {
-		console.log('View was loaded');
-	},
+		var self = this;
 
-	':resized': function(width, height) {
-		console.log('View was resized to ' + width + 'x' + height);
-	},
+		self.selection = self.keySelectionWithItems([
+				self.get('ibTanggal'),
+				self.get('ibBulan'),
+                                self.get('ibTahun'),
+				self.get('button-box').get('bSearch')
+			],
+			{
+				focusedCallback: function(item) {
+					if (item instanceof ImageView) {
+						self.get('button-box').get('bSearch').src(app.resourceURL('search_ho.png'));
+					} else {
+						self.get('button-box').get('bSearch').src(app.resourceURL('search.png'));
+					}
 
-	':keydown': function(key) {
-		console.log('Key down: '+ key);
-	},
+				}
+			});
 
-	':keyup': function(key) {
-		console.log('Key up: ' + key);
-	},
+		self.get('button-box').get('bSearch').src(app.resourceURL('search_ho.png'));
+		self.get('button-box').get('bSearch').src(app.resourceURL('search.png'));
 
-	':keypress': function(key) {
-		console.log('Key press: ' + key);
-	},
+		self.get('ibTanggal').on('activate', function() {
+			self.get('ibTanggal').emit('keypress', 'fire');
+		});
 
-	':active': function() {
-		console.log('View is active');
-	},
+		self.get('ibBulan').on('activate', function() {
+			self.get('ibBulan').emit('keypress', 'fire');
+		});
+                self.get('ibTahun').on('activate', function() {
+			self.get('ibTahun').emit('keypress', 'fire');
+		});
 
-	':inactive': function() {
-		console.log('View is inactive');
+		self.get('button-box').get('bSearch').on('activate', function() {
+			app.msg('search', {tg: self.get('ibTanggal').value(), bl: self.get('ibBulan').value(),th: self.get('ibTahun').value()});
+		});
+                
+		app.on('connected', function() {
+			console.log('Connected to backend.');
+
+			app.on('message', function(action, data){
+				if (action === 'search') {
+					//console.log(data.Title);
+					app.setContent('detail', data);
+				}
+			});
+		});
 	}
 });
